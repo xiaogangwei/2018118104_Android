a8bb67a68bda839e6555759d7aba24d28310a8e1
DESKTOP-BRFH2D9\98261 <2596267622@qq.com>
2020-10-05 11:06:34 +0800

    提交第二次作业初稿

---
+++ MyApplication02/app/src/main/java/com/example/myapplication/singleinstance.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class singleinstance extends AppCompatActivity {
+    static  String num="Singleinstance";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singleinstance);
+        Log.d("singleinstance","Task id is"+getTaskId());
+        Button button1=(Button)findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+            }
+        });
+        Button button2=(Button)findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(singleinstance.this,singleTask.class);
+                //intent.setData(Uri.parse("tel:10000"));
+                startActivity(intent);
+            }
+        });
+    }
+}
