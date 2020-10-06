801259a3e769ec6a08539c974a833bc6fc9b8243
DESKTOP-BRFH2D9\98261 <2596267622@qq.com>
2020-10-06 11:00:08 +0800

    singleTask,singleinstance

---
+++ MyApplication02/app/src/main/java/com/example/myapplication02/singleTask.java
@@ -0,0 +1,43 @@
+package com.example.myapplication02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class singleTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_task);
+        Button button=(Button)findViewById(R.id.button_3);
+        Button button1=(Button)findViewById(R.id.button_4);
+        Log.d("singleTask",this.toString());
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(singleTask.this,MainActivity.class);
+                //intent.setData(Uri.parse("tel:10000"));
+                startActivity(intent);
+                startActivity(intent);
+            }
+        });
+    }
+}
