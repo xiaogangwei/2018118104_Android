aa58fdb0877e03b09b0ff6ed9748340c991d52b9
DESKTOP-BRFH2D9\98261 <2596267622@qq.com>
2020-10-06 10:31:10 +0800

    MainActivity

---
+++ MyApplication02/app/src/main/java/com/example/myapplication02/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.myapplication02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    static String num="MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.button_1);
+        Log.d("MainActivity",this.toString());
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"点击",Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button1=(Button)findViewById(R.id.button_2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.button_2:Intent intent2=new Intent(MainActivity.this,singleinstance.class);
+                        //intent2.setData();
+                        startActivity(intent2);break;
+                    case R.id.button_1:
+                        Toast.makeText(MainActivity.this,"点击",Toast.LENGTH_SHORT).show();
+                        Intent intent=new Intent(MainActivity.this,MainActivity.class);
+                        startActivity(intent); break;
+
+                }
+
+            }
+        });
+    }
+}
